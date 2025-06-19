8321b58cf4bae92d976d0546a9f5c19b8a416dda
devan191 <devan23191@iiitd.ac.in>
2025-06-19 19:01:10 +0530

    initial commit

---
+++ app/my-journey/page.tsx
@@ -0,0 +1,8 @@
+
+const Profile = () => {
+  return (
+    <div>ProfilePage</div>
+  )
+}
+
+export default Profile
---
+++ app/subsription/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Subsription = () => {
+  return (
+    <div>Subscription</div>
+  )
+}
+
+export default Subsription
