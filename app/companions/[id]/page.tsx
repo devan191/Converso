7421fb1092e6d4de80d56e625d2beb97a92311f8
devan191 <devan23191@iiitd.ac.in>
2025-06-21 05:58:06 +0530

    implement call workflow

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -3,6 +3,7 @@ import { getSubjectColor } from "@/lib/utils";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import Image from "next/image";
+import CompanionComponent from "@/components/CompanionComponent";
 
 interface CompanionSessionPageProps {
   params: Promise<{ id: string }>;
@@ -48,6 +49,13 @@ const CompanionSession = async ({ params }: CompanionSessionPageProps) => {
           {duration} minutes
         </div>
       </article>
+
+      <CompanionComponent
+        {...companion}
+        companionId={id}
+        userName={user.firstName!}
+        userImage={user.imageUrl!}
+      />
     </main>
   );
 }
--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -10,7 +10,7 @@ const NewCompanion = async () => {
   return (
     <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
       <article className="w-full gap-4 flex flex-col">
-        <h1>Companion Builder</h1>
+        <h1 key="heading">Companion Builder</h1>
         <CompanionForm />
       </article>
     </main>
